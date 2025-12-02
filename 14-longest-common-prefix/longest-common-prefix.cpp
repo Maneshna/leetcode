0e720caa45ef9c692e49a2f5901bed37b9d540c6
Maneshna <135442163+Maneshna@users.noreply.github.com>
2025-12-02 21:05:08 +0530

    longest-common-prefix.cpp

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,9 +3,9 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
         for(int i =0; i<strs[0].size(); i++){
-            char c = strs[0][i];
-            for(int j = 1; j<strs.size(); j++){
-                if(i>=strs[j].size() || strs[j][i] !=c){
+            char c = strs[0][i]; //we start the iteration from the first string value and map them
+            for(int j = 1; j<strs.size(); j++){ //now this first string is compared with every other string in the array.
+                if(i>=strs[j].size() || strs[j][i] !=c){ //is there is common and then suddenly the matching is stopped then we stop and print from 0 index to the i index where we iterated in the first place
                     return strs[0].substr(0, i );
                 }
             }
