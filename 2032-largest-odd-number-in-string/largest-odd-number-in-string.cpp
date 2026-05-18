7123a53fb795248ea074e62d7ac194e8771669ae
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-05-18 19:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.30%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
+        int index = -1;
+
         int n = num.length();
-        int ind =-1;
-        for(int i = n-1; i>=0; i--){
-            if((num[i]- '0') %2 ==1){
-                ind = i;
+        for(int i =n; i>=0; i--){
+            if((num[i]-'0')%2==1){
+                index = i;
                 break;
             }
         }
-        int i=0;
-        while(i <= ind &&num[i] == '0'){
+
+        int i =0;
+        while(i<=n && num[i]==0){
             i++;
         }
-        
-        return num.substr(i, ind-i+1);
+
+        return num.substr(i, index-i+1);
+
         
     }
 };
