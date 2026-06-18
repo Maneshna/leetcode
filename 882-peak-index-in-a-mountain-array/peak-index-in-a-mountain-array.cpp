4c9ebefabbf6d0c2c7aebca2a8451cbb18cfe46b
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-06-18 23:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (47.92%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& nums) {
-
-        //brute force
-
-        //we start with the first thing is that traversing through the array and seeing whether the next number is greater than the current or not. as soon as this is not satisfied, we return the index.
+        //now we approach with the the optimal solution. since anything that says search, binary search can be applied so let's see. 
+        //we can split the array in two. then work on the fact that the mid one might end up in the increasing side of the mountain. this way we will move to the right side. if not then left side.
 
         int n = nums.size();
-        
-        for(int i =0 ; i<n; i++){
-            if(nums[i]>nums[i+1]){
-                return i;
+        int low=0;
+        int high=n-1;
+
+        while(low<high){
+            int mid = low+ (high - low)/2;
+
+            if(nums[mid] < nums[mid+1]){
+                //this means we are in the increasing side of the array so we move to the right side to find the peak
+
+                low = mid +1;
+            }
+            else{
+                //it means we are on the decreasing side of the array so we work on the left side instead
+
+                high = mid;
             }
         }
-        return -1;
+        return low;
+
         
     }
 };
