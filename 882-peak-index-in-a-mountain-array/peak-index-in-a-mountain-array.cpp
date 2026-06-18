6e8751ea0dc6322f94c6dec947c30e0b3579ad93
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-06-18 23:51:16 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (10.69%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        int max = 0;
-        for(int i =0; i <n; i++){
-            if(arr[i]>=arr[max]){
-                max = i;}
-                            
+    int peakIndexInMountainArray(vector<int>& nums) {
+
+        //brute force
+
+        //we start with the first thing is that traversing through the array and seeing whether the next number is greater than the current or not. as soon as this is not satisfied, we return the index.
+
+        int n = nums.size();
+        
+        for(int i =0 ; i<n; i++){
+            if(nums[i]>nums[i+1]){
+                return i;
+            }
         }
-        return max;
+        return -1;
         
     }
 };
