c2bd481589d884345c769630a5cd97cf56c01e6d
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-06-28 20:08:01 +0530

    Time: 35 ms (77.40%) | Memory: 39.5 MB (96.28%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        //we will start by  brute force approch of using sliding window in this.
         int n = nums.size();
-        int totalsubarrays = 0;
-        for(int i =0; i<n; ++i){
-            int targetcount =0;
+        int  countsubarrays = 0;
+        for(int i = 0; i<n; i++){
+            int targetcount = 0;
+            for (int j =i; j<n; j++){
 
-            for(int j =i; j<n; ++j){
-                if(nums[j]==target){
+                if(nums[j] == target){
                     targetcount++;
                 }
-                int windowsize =j-i+1;
-
-                if(targetcount > windowsize/2){
-                    totalsubarrays++;
+                int windowsize = j-i+1;
+                if(targetcount > windowsize /2){
+                    countsubarrays++;
                 }
             }
         }
-        return totalsubarrays;
+        return countsubarrays;
         
     }
 };
