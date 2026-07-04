4d78becdb22018d90f314c300710a5fc46e91b13
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-07-05 01:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (80.56%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
 
         int insertposition =1; //we will check from 2nd index to the one just before it so we skipped the index 0 aka 1st one
         for(int i =1; i<n; i++){
-            if(nums[i] != nums[insertposition-1]){
+            if(nums[i] != nums[insertposition-1]){ //they are not same means they are unique so we replace the nums at the 
                 nums[insertposition] = nums[i];
                 insertposition++;
             }
