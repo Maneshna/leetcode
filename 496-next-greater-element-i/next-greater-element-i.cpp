01f100f2429456385c9c7f687867dcdde5c25fdc
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-07-18 01:06:21 +0530

    Time: 2 ms (46.95%) | Memory: 12.9 MB (39.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,27 +4,28 @@ public:
         unordered_map<int, int> next_greater;
         stack<int> st;
 
-        // Traverse nums2 from right to left
+       
+       
         for (int i = nums2.size() - 1; i >= 0; --i) {
             int num = nums2[i];
 
-            // Pop elements that are smaller than or equal to the current element
+           
             while (!st.empty() && st.top() <= num) {
                 st.pop();
             }
 
-            // If stack is empty, no greater element exists to the right
+          
             if (st.empty()) {
                 next_greater[num] = -1;
             } else {
                 next_greater[num] = st.top();
             }
 
-            // Push current element to stack for elements on its left
+            
             st.push(num);
         }
 
-        // Build the result for nums1 using the map
+      
         vector<int> ans;
         for (int num : nums1) {
             ans.push_back(next_greater[num]);
