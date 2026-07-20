cc855f94d6dba4110facff5102c9ce260265f82c
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-07-20 19:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (76.59%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        //the core idea in this is that we traverse through the bacskets one and assign the first one that is more than the fruits one and then mark it -1 and also change the bool value in the fruits one to true since it got placed. if it not placed then we obviously count in the the lower if statement of not placed.
-        int n =fruits.size();
+        int n = fruits.size();
         int unplaced =0;
-
-        for(int i=0;i<n;++i){
+        for(int i = 0; i<n; i++){
             bool placed = false;
-            for(int j = 0;j<n;++j){
+            for(int j =0; j<n;j++){
                 if(baskets[j]>=fruits[i]){
-                    baskets[j]=-1; //this will always less than any number so the already asssigned basket will not be assigned to the fruits in any case 
-                    placed =true;
+                    baskets[j]=-1;
+                    placed = true;
                     break;
-
                 }
             }
             if(!placed){
