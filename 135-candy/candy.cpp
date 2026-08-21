d59ad5ab46dc32c8e9a65369d64381da075c5328
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-08-21 16:27:57 +0530

    Time: 7 ms (9.91%) | Memory: 71.3 MB (6.00%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n,1);
+        
+
+        //first pass 
+        for(int i =1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i] = candies[i-1] +1;
+            }
+        }
+        //second pass
+        for(int i = n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1] +1);
+            }
+        }
+        int sum =0;
+        for(int c:candies){
+            sum += c;
+        }
+        return sum;
+
+        
+    }
+};
