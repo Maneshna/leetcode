d7884ab636419e6fee7015c8d71a9b1c6c768970
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-08-22 16:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (6.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool isValid(string s) {
+        //for this we use stack 
         stack<char> st;
-
-        for (char c : s) {
-           
-            if (c == '(' || c == '{' || c == '[') {
+        for(int c:s){
+            if(c=='['|| c=='{' || c=='('){
                 st.push(c);
-            } 
-            else {
-                
-                if (st.empty()) return false;
+            }
+            else{
+
+                if(st.empty()) return false; //this means there is no opening parenthsis so it can never be valid 
 
                 char top = st.top();
                 st.pop();
+                //now we pop the top elements (which should be the opnening parenthesis so we will pop and compare with the remining ones in the array)
 
-                
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
+                if((c==')' && top != '(')||
+                (c=='}' && top != '{') ||
+                (c==']' && top!= '[')){
                     return false;
                 }
+
             }
         }
-
         return st.empty();
+
+        
     }
 };
