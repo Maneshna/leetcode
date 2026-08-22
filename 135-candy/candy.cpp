22b5f14d4a4dad6d7f053775ef03973f8964712e
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-08-22 15:58:50 +0530

    Time: 11 ms (6.65%) | Memory: 71.4 MB (6.91%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        //double pass
         int n = ratings.size();
-        vector<int> candies(n,1);
-        
+        vector<int>candies(n,1); //everyone has to get one
 
-        //first pass 
-        for(int i =1; i<n; i++){
+        //first pass
+        for(int i =1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
                 candies[i] = candies[i-1] +1;
             }
+
         }
         //second pass
-        for(int i = n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                candies[i] = max(candies[i], candies[i+1] +1);
+        for(int i =n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1]+1);
             }
         }
-        int sum =0;
-        for(int c:candies){
-            sum += c;
-        }
-        return sum;
 
+        //sum the candies
+        int total =0;
+        for(int c:candies){
+            total+=c;
+        }
+        return total;
         
     }
 };
