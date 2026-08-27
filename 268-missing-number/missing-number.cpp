6ebad7550dad1b442bc4b245dabaccf32a1fb24d
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-08-27 16:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.15%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,15 +3,17 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         vector<bool> seen(n+1, false);
+
         for(int num: nums){
             seen[num] = true;
         }
-        for(int i =0; i<=n;i++){
+        for(int i =0; i<=n;i++){ //iterate through numbers 
             if(!seen[i]){
                 return i;
             }
         }
         return -1;
+
         
     }
 };
