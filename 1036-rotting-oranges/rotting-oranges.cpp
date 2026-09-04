56c60e417241502f6068169240495434790a717c
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-09-04 23:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.07%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int,int>> q;
+
+        int fresh =0;
+        for(int i =0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
+        int minutes = 0;
+        int directions[4][2] = {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
+        while(!q.empty() && fresh >0){
+            int size = q.size();
+            for(int i =0;i<size; i++){
+                auto[r,c] = q.front();
+                q.pop();
+            
+            for(auto& dir : directions){
+                int nr = r+dir[0];
+                int nc = c+dir[1];
+
+                if(nr >= 0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                    grid[nr][nc] = 2;
+                    fresh--;
+                    q.push({nr,nc});
+                }
+            }
+        }
+            minutes++;  
+        }
+
+        if(fresh >0){
+            return -1;
+
+        }
+        return minutes;
+        
+
+
+        
+    }
+};
