013e7db56fc30bced0b2863d4d59e83804b5a208
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-09-18 07:15:13 +0530

    Time: 4 ms (19.67%) | Memory: 42.2 MB (5.03%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& num) {
-        unordered_map<int,int> freq;
-        int n = num.size(); 
-        for(int number: num){
-            freq[number]++;
-            
-            if(freq[number]>n/2){
-            return number;
-        }
+    int majorityElement(vector<int>& nums) {
+        //make a map to count the element that appears more then n/2 times
+        unordered_map<int, int> freq;
+        int n =nums.size();
+        for(int num: nums){
+            freq[num]++;
+            if(freq[num]>n/2){
+                return num;
+            }
         }
         return 0;
         
