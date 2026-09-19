c256d423c37396668bf75f281a5ac5f98f664fd4
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2026-09-19 10:54:38 +0530

    Time: 12 ms (72.44%) | Memory: 19.2 MB (62.74%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<int> window;
+        vector<int> lastSeen(256, -1);
+        int maxLength = 0;
         int left =0;
-        int maxlength = 0;
 
-        for(int right = 0; right<s.size(); right++){
-            //now if the letter is already there and mapped in the hashmap we move left++ and also drop the mapping of that letter from the hashmap
+        for(int right =0; right<s.length(); right++){
+            char currentChar =s[right];
 
-            while(window.count(s[right])){
-                window.erase(s[left]);
-                left++; //this means that the next element coming is the one that already exsisted so we need to remove the left most from the hashmap and also shift the window
+            if(lastSeen[currentChar]>=left){
+                left=lastSeen[currentChar] +1;
             }
-            window.insert(s[right]);
-            //regardless whether the letter was mapped before and remove or it was unique, we need t map it now so that is why it is not written as an if else statement.
-
-            //now we find the max element
-            maxlength = max(maxlength, right-left+1);
+            lastSeen[currentChar]=right;
+            maxLength = max(maxLength, right-left+1);
         }
-        return maxlength;
-
-
-        
+        return maxLength;
     }
 };
